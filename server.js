3ea8e4e6a281f621c722168209fecb1738ef9b27
niksolaz <solazzo.nicola@gmail.com>
2015-09-18 19:25:34 +0200

    setted app

--- server.js
+++ server.js
@@ -14,3 +14,25 @@ var session = require('express-session');
 
 var configDB = require('./config/database.js');
 mongoose.connect(configDB.url); // connect database
+
+// set application express
+app.use(morgan('dev')); //log req to the console
+app.use(cookieParser()); //read cookies( auth) 
+app.use(bodyParser()); //get info html
+
+app.set('view engine','ejs'); // set templates
+
+//set for passport
+app.use(session({
+				secret: process.env.SECRET_MODULUS,
+				resave: true,
+				saveUninitialized: true
+				}));  // session secret
+app.use(passport.initialize());
+app.use(passport.session()); //persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+require('./app/routes.js').(app,passport); //load routes and pass app in passport
+
+app.listen(port);
+console.log('server is connected in localhost:3000 ' + port);
